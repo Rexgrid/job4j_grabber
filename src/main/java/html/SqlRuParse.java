29adf51912b1f9caa97d2e7fbe9d0062d416284d
Rexgrid <daervia93@gmail.com>
2021-10-17 02:11:16 +0300

    Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -5,13 +5,30 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element parent = td.parent();
-            System.out.println(parent.child(5).text());
+        int counter = 1;
+        List<String> pages = new ArrayList<>();
+        pages.add("https://www.sql.ru/forum/job-offers");
+        Document doc = Jsoup.connect(pages.get(0)).get();
+        Elements page = doc.select("#content-wrapper-forum > table:nth-child(7) > tbody > tr > td:nth-child(1) > a");
+        for (Element pg : page) {
+            pages.add(pg.attr("href"));
         }
+        for (int i = 0; i < 5; i++) {
+        Document doc1 = Jsoup.connect(pages.get(i)).get();
+            System.out.println("Страница: " + counter);
+           Elements row = doc1.select(".postslisttopic");
+            for (Element td : row) {
+                Element parent = td.parent();
+                System.out.println(parent.child(5).text());
+            }
+            counter++;
+        }
+
     }
 }
