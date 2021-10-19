2f1bfe02b526eb85b841fb802a150a8feb7a2b1f
Rexgrid <daervia93@gmail.com>
2021-10-19 21:33:26 +0300

    Парсинг https://www.sql.ru/forum/job-offers/3 [#285210 #216954]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -11,23 +11,15 @@ import java.util.List;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        int counter = 1;
-        List<String> pages = new ArrayList<>();
-        pages.add("https://www.sql.ru/forum/job-offers");
-        Document doc = Jsoup.connect(pages.get(0)).get();
-        Elements page = doc.select("#content-wrapper-forum > table:nth-child(7) > tbody > tr > td:nth-child(1) > a");
-        for (Element pg : page) {
-            pages.add(pg.attr("href"));
-        }
-        for (int i = 0; i < 5; i++) {
-        Document doc1 = Jsoup.connect(pages.get(i)).get();
-            System.out.println("Страница: " + counter);
-           Elements row = doc1.select(".postslisttopic");
+        String url = "https://www.sql.ru/forum/job-offers";
+        for (int i = 1; i < 6; i++) {
+            Document doc = Jsoup.connect(url + "/" + i).get();
+            System.out.println("Страница: " + i);
+            Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
                 Element parent = td.parent();
                 System.out.println(parent.child(5).text());
             }
-            counter++;
         }
 
     }
