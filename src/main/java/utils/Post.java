b81eb631dae24da1b6a74a2d9070b8b6341c68f6
Rexgrid <daervia93@gmail.com>
2021-10-19 22:09:41 +0300

    Модель данных - Post. [#285211]

---
+++ src/main/java/utils/Post.java
@@ -0,0 +1,55 @@
+package utils;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Post pt = new Post(1,"asd", "asd", "aaa", LocalDateTime.now());
+        Post pt1 = new Post(1,"asd", "asd", "aaa", LocalDateTime.now());
+        System.out.println(pt.equals(pt1));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id &&
+                Objects.equals(title, post.title) &&
+                Objects.equals(link, post.link) &&
+                Objects.equals(description, post.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, link, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Vacancy{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", link='" + link + '\'' +
+                ", description='" + description + '\'' +
+                ", created=" + created +
+                '}';
+    }
+}
